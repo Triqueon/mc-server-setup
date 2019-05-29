3a7f4ccb9b422b534983e0bc43cb0b054c22453f
Pascal Stein <pascal@neocosmo.de>
2019-05-29 10:30:49 +0200

    Added license acception to teamspeak script

--- teamspeak.sh
+++ teamspeak.sh
@@ -4,6 +4,7 @@ sudo adduser --disabled-login teamspeak
 wget -O teamspeak3-server.tar.bz2 http://dl.4players.de/ts/releases/3.7.1/teamspeak3-server_linux_amd64-3.7.1.tar.bz2
 tar -jxvf teamspeak3-server.tar.bz2
 sudo mv teamspeak3-server_linux_amd64 /usr/local/teamspeak
+sudo touch /usr/local/teamspeak/.ts3server_license_accepted
 sudo chown -R teamspeak:teamspeak /usr/local/teamspeak
 sudo cp ts3.service.default /etc/systemd/system/ts3.service
 sudo ufw allow 9987/udp
