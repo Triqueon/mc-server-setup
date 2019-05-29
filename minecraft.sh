e15be3dbfaeb6d3983b46a54f943a0a38ac6a99e
Pascal Stein <pascal.stein@gmail.com>
2019-05-29 15:57:00 +0200

    Fixed several typos and added forgotten mkdir

--- minecraft.sh
+++ minecraft.sh
@@ -2,7 +2,8 @@
 
 sudo adduser --disabled-login minecraft
 wget -O server.jar https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar
-sudo mv server.jar /usr/local/minecraft
+sudo mkdir /usr/local/minecraft
+sudo mv server.jar /usr/local/minecraft/
 echo "eula=true" > eula.txt
 sudo mv eula.txt /usr/local/minecraft
 sudo chown -R minecraft:minecraft /usr/local/minecraft
