1fafcb8aadfd83709ca80909ba79ea959d6feda2
Pascal Stein <pascal.stein@gmail.com>
2020-03-26 13:46:16 +0100

    Added basic terraria scripts

---
+++ terraria.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+VERSION=1353
+sudo adduser --disabled-login terraria
+wget -O terraria-server.zip http://terraria.org/server/terraria-server-${VERSION}.zip
+unzip terraria-server.zip
+rm -rf ${VERSION}/Windows
+rm -rf ${VERSION}/Mac
+sudo mv ${VERSION} /usr/local/terraria
+sudo chown -R terraria:terraria /usr/local/terraria
+sudo cp terraria.service.default /etc/systemd/system/terraria.service
+sudo ufw allow 7777
+sudo -u terraria /usr/local/terraria/TerrariaServer.bin.x86_64
+sudo systemctl daemon-reload
+sudo systemctl enable terraria.service
+sudo systemctl start terraria
+sudo systemctl status terraria
