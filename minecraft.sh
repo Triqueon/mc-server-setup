05a506e38a5710eab5e73677ab11a6de8d882e24
Pascal Stein <pascal.stein@gmail.com>
2020-03-29 17:38:02 +0200

    Added some documentation and sanity checks

--- minecraft.sh
+++ minecraft.sh
@@ -1,12 +1,22 @@
 #!/bin/sh -e
 
-sudo adduser --disabled-login minecraft
+#Add User
+if ! id "minecraft" >/dev/null 2>&1; then
+  sudo adduser --disabled-login minecraft
+fi
+
+#Download JAR File. This may need to updated for the current version
 wget -O server.jar https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar
 sudo mkdir /usr/local/minecraft
 sudo mv server.jar /usr/local/minecraft/
+
+#Accept EULA
 echo "eula=true" > eula.txt
 sudo mv eula.txt /usr/local/minecraft
+
 sudo chown -R minecraft:minecraft /usr/local/minecraft
+
+#Copy service file and load the new service
 sudo cp minecraft.service.default /etc/systemd/system/minecraft.service
 sudo systemctl daemon-reload
 sudo systemctl enable minecraft.service
